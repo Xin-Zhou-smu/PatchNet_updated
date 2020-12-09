99e2a1541fa85fce89e7303725d459e371899f0b
Xin-Zhou-smu <70464742+Xin-Zhou-smu@users.noreply.github.com>
2020-12-09 19:49:11 +0800

    Update text2dict.py
    change the input way of parameters

--- text2dict.py
+++ text2dict.py
@@ -5,19 +5,34 @@ Created on Wed Dec  9 14:25:28 2020
 @author: xinzhou.2020
 """
 
-## Function: Read .out file, change its format and store in .pkl file
-## Input:  path of "*.out" (path_input_data)
-## Output: path of "*.pkl" (path_output_data)
+## Note that you need to change the path_data to the path of "*.out"
+## Input:  path of "*.out" 
+## Output: path of "*.pkl"
 
-
+import argparse
 from preprocessing import extract_commit
 import pickle
 
+def read_args():
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('-text_path', type=str, help='the directory of text data')
+    parser.add_argument('-dict_path', type=str, help='the path the generated data want to save')
+    parser.add_argument('-print', type=bool,default = False, help='whether to print some example of generated data')
+
+    return parser
+
+
 if __name__ == "__main__": 
     
-    path_input_data = "test_data.out"
-    path_output_data = "./try_data/test_try.pkl"
-    show_data = True                    # print the generate data or not
+    params = read_args().parse_args() 
+    
+    #path_input_data = "test_data.out"
+    #path_output_data = "./try_data/test_try.pkl"
+    path_input_data = params.text_path
+    path_output_data = params.dict_path
+    show_data = params.print                # print the generate data or not
     
     #read commits and change format into dict
     commits_ = extract_commit(path_file=path_input_data)
